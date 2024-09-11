969e1c3b2aebc292bdad4f196da7df09facb1903
Alex000291 <139620699+Alex000291@users.noreply.github.com>
2024-09-11 17:50:19 +0800

    Delete temp.js

--- temp.js
+++
@@ -1,7 +0,0 @@
-const bitcore = require('bitcore-lib');
-
-const privateKey = bitcore.PrivateKey();
-const publicKey = privateKey.toPublicKey();
-const address = privateKey.toAddress().toString();
-
-console.log(privateKey.toString(), publicKey.toString(), address);
